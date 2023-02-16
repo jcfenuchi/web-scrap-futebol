e056c7f75d6cf6d014cd2154be25751ba5b4f2be
jcfenuchi <juliocesarfenuchi@hotmail.com>
2023-02-16 13:08:17 -0300

    resolv bugs

--- scrap.py
+++ scrap.py
@@ -4,35 +4,39 @@ from playwright.sync_api import sync_playwright
 class Scraper_Master():
     def __init__(self,arquivo):
         self.arquivo = arquivo
+        self.arquivo.write("nome,posicao,nascionalidade,idade,clube,melhor_marca_da_carreira,ultima_alteracao,valor_de_mercado\n")
+
         self.cont = 1
 
     def scraper(self,link):
         with sync_playwright() as p:
-            browser = p.firefox.launch()
+            browser = p.chromium.launch()
             page = browser.new_page()
             page.goto(link)
             conteudo = page.content()
             page.close()
         return conteudo
+    
 
     def start(self):
-        fist_scrap = self.scraper("https://www.transfermarkt.com.br/campeonato-brasileiro-serie-a/marktwerte/wettbewerb/BRA1/pos//detailpos/0/altersklasse/alle/plus/1")
+        print(f"Iniciando o arquivo {self.arquivo.name}")
+        print("Fazendo Scrap da pagina 1 do site")
+        fist_scrap = self.scraper("https://www.transfermarkt.com.br/campeonato-brasileiro-serie-a/marktwerte/wettbewerb/BRA1/ajax/yw1/pos//detailpos/0/altersklasse/alle/plus/1")
         scrap = bs(fist_scrap,'html.parser')
-        print("Fazendo Scrap da pagina 01")
         self.scrap_jogadores(fist_scrap)
         a = scrap.find_all("li", {"class":"tm-pagination__list-item tm-pagination__list-item--icon-last-page"})[0].find("a").get("href").split("/")
         for i in range(2,int(a[-1])+1):
             link_base = "/".join(a[:-1])
-            print(f"Fazendo Scrap da Pagina {i}")
-            self.scrap_jogadores(self.scraper("https://www.transfermarkt.com.br"+link_base+str(i)))
-        print(f"Arquivo {self.file.name} foi terminado.")
+            print(f"Fazendo Scrap da Pagina {i} do site")
+            print("https://www.transfermarkt.com.br"+link_base+"/"+str(i))
+            self.scrap_jogadores(self.scraper("https://www.transfermarkt.com.br"+link_base+"/"+str(i)))
+            print("#"*25)
+            print(f"Arquivo {self.arquivo.name} foi terminado.")
     def scrap_jogadores(self,scrap_content):
         scrap = bs(scrap_content,'html.parser')
         jogadores = scrap.find_all("tbody")[1].find_all("tr")
         infos = [info.find_all("td") for info in jogadores]
-        
-        self.arquivo.write("nome,posicao,nascionalidade,idade,clube,melhor_marca_da_carreira,ultima_alteracao,valor_de_mercado\n")
-        
+        print(f"separando dados e escrevendo no {self.arquivo.name}")
         for filter in infos:
             if len(filter) == 11:
                 nome = filter[1].find_all("a")[0].text
